543a9b4eda382e9fce13f686f087eadb00afdc51
andriell <root.andriell@gmail.com>
2015-12-23 11:24:16 +0300

    start

---
+++ src/com/apress/prospring4/ch5/WorkerBean.java
@@ -0,0 +1,16 @@
+package com.apress.prospring4.ch5;
+
+/**
+ * Created by Андрей on 23.12.2015.
+ */
+public class WorkerBean {
+    public void doSomeWork(int noOfTimes) {
+        for (int х = 0; х < noOfTimes; х++) {
+            work();
+        }
+    }
+
+    private void work() {
+        System.out.print("");
+    }
+}
